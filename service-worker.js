ed5d5faa17ce321ea5c5fb733b44c5148410601a
Jai Nijhawan <jainijhawan@Jais-MBP-2.ht.home>
2023-06-03 16:09:44 -0400

    Added Cache with network Fallback

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,53 @@
+const cacheName = 'cacheAssets-v7'
+
 
 self.addEventListener('install', (event) => {
-    console.log('[SW] Install!:', event);
     self.skipWaiting();
+    console.log('[SW] Install!:', event);
+    event.waitUntil(
+        caches.open(cacheName)
+            .then((cache) => {
+                console.log('Cache:', cache);
+                cache.addAll
+                cache.add('/')
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/script.js',
+                    '/style.css',
+                    '/registerServiceWorker.js',
+                    '/music.jpg',
+                    '/manifest.json',
+                    '/icons/android-chrome-192x192.png'
+                ])
+            })
+            .catch((error) => {
+                console.log('Cache failed:', error);
+            })
+    );
 });
 
 self.addEventListener('activate', (event) => {
     console.log('[SW] Activate:', event);
     event.waitUntil(clients.claim());
+
+    event.waitUntil(caches.keys().then(function (cacheNames) {
+        return Promise.all(cacheNames
+            .filter(item => item !== cacheName)
+            .map(item => caches.delete(item))
+            );
+    }))
 });
 
-self. addEventListener ('fetch', () => {
-    return;
+self.addEventListener('fetch', (event) => {
+
+    event.respondWith(
+        caches.open(cacheName)
+            .then((cache) => {
+                return cache.match(event.request)
+                    .then((response) => {
+                        return response || fetch(event.request);
+                    })
+            })
+    );
 });
